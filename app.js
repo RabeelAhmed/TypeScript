e0162a5e8c4d84432201f39ff2dd12ed6797c69f
RabeelAhmed <rabeelsulehria3@gmail.com>
2024-09-28 14:12:31 +0500

    Add TypeScript Type Aliases and Intersection Types examples

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-// Interface Concept <==============
-// A function 'abcd' that accepts an object of type 'user'
-function abcd(obj) {
-    console.log(obj.name, obj.age, obj.email);
+"use strict";
+// // Interface Concept <==============
+let a;
+a = "Rabeel";
+console.log(typeof (a));
+let b;
+b = 22;
+console.log(typeof (b));
+// A function 'abcd' that accepts an object of type 'Admin'.
+function abcd(a) {
+    console.log(a.email);
 }
-// Calling the 'abcd' function with an object that conforms to the 'user' interface.
-abcd({ name: "Rabeel", email: "rabeel@gmail.com", age: 22 });
-// A function 'details' that accepts an object of type 'Admin'.
-function details(obj) {
-    console.log(obj.isAdmin, obj.name, obj.email);
-}
-// Calling the 'details' function with an object that conforms to the 'Admin' interface.
-details({ isAdmin: true, name: "Rabeel", email: "rabeel@gmail.com", age: 22 });
--- app.ts
+++ app.ts
@@ -1,38 +1,81 @@
-// Interface Concept <==============
+// // Interface Concept <==============
 
-// Defining an interface 'user' which acts as a blueprint for an object structure.
-interface user {
-    name: string,     
-    email: string,   
-    age: number  
+// // Defining an interface 'user' which acts as a blueprint for an object structure.
+// interface user {
+//     name: string,     
+//     email: string,   
+//     age: number  
+// }
+
+// // A function 'abcd' that accepts an object of type 'user'
+// function abcd(obj: user) {
+//     console.log(obj.name, obj.age, obj.email);
+// }
+
+// // Calling the 'abcd' function with an object that conforms to the 'user' interface.
+// abcd({ name: "Rabeel", email: "rabeel@gmail.com", age: 22 });
+
+
+// // Extended Interface Concept <==============
+
+// // Redefining the 'user' interface to include just 'name' and 'email' properties.
+// interface user {
+//     name: string, 
+//     email: string 
+// }
+
+// // Defining a new interface 'Admin' that extends the 'user' interface.
+// interface Admin extends user {
+//     isAdmin: boolean 
+// }
+
+// // A function 'details' that accepts an object of type 'Admin'.
+// function details(obj: Admin) {
+//     console.log(obj.isAdmin, obj.name, obj.email);
+// }
+
+// // Calling the 'details' function with an object that conforms to the 'Admin' interface.
+// details({ isAdmin: true, name: "Rabeel", email: "rabeel@gmail.com", age: 22 });
+
+
+// Type Aliases Concept <==============
+
+// Creating a type alias 'value' which represents the 'String' type.
+type value = String
+
+let a: value
+
+a = "Rabeel"
+
+console.log(typeof(a))
+
+//  OR
+
+// Creating a type alias 'num' which can be either 'String' or 'number'.
+type num = String | number
+
+let b: num
+
+b = 22
+
+console.log(typeof(b))
+
+
+// Intersection Types Concept <==============
+
+// Defining a 'user' type with 'name' and 'email' properties.
+type user = {
+    name: String, 
+    email: String, 
 }
 
-// A function 'abcd' that accepts an object of type 'user'
-function abcd(obj: user) {
-    console.log(obj.name, obj.age, obj.email);
+// Defining an 'Admin' type that is an intersection of 'user' and an additional method.
+type Admin = user & {
+    getDetails(user: String): void
 }
 
-// Calling the 'abcd' function with an object that conforms to the 'user' interface.
-abcd({ name: "Rabeel", email: "rabeel@gmail.com", age: 22 });
-
-
-// Extended Interface Concept <==============
-
-// Redefining the 'user' interface to include just 'name' and 'email' properties.
-interface user {
-    name: string, 
-    email: string 
+// A function 'abcd' that accepts an object of type 'Admin'.
+function abcd(a: Admin) {
+    console.log(a.email)
 }
 
-// Defining a new interface 'Admin' that extends the 'user' interface.
-interface Admin extends user {
-    isAdmin: boolean 
-}
-
-// A function 'details' that accepts an object of type 'Admin'.
-function details(obj: Admin) {
-    console.log(obj.isAdmin, obj.name, obj.email);
-}
-
-// Calling the 'details' function with an object that conforms to the 'Admin' interface.
-details({ isAdmin: true, name: "Rabeel", email: "rabeel@gmail.com", age: 22 });
